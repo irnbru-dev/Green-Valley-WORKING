fec9e0751235d6dc776d74f3000f53ef7b545849
irnbru-dev <darina.irnbru@gmail.com>
2016-12-22 15:39:05 +0300

    Home-page
    rest-type-switcher fix, offers-carousel

--- js/script.js
+++ js/script.js
@@ -25,3 +25,46 @@ $('#gallery-thumbs a').on('click', function(e){
 })
 
 });
+
+
+// Rest-type-switcher табы home-page
+
+var tabs = document.querySelectorAll('.rest-type-switcher__tabs .rest-type-switcher__tab-item a');
+var restType = document.querySelectorAll('.rest-type-switcher__type .rest-type-switcher__type-item');
+
+function changeTab(event) {
+  event.preventDefault();
+  for (var i=0; i<tabs.length; i++) {
+    tabs[i].parentNode.classList.remove('rest-type-switcher__is-active');
+  }
+  event.target.parentNode.classList.add('rest-type-switcher__is-active');
+  for (var i=0; i<tabs.length; i++) {
+    restType[i].classList.remove('rest-type-switcher__is-active');
+  }
+
+var link = event.target.getAttribute('href');
+document.querySelector(link).classList.add('rest-type-switcher__is-active');
+}
+
+for (var i=0; i<tabs.length; i++) {
+  tabs[i].addEventListener('click', changeTab);
+};
+
+
+// Sales-offers owl-carousel
+  $('#offers').owlCarousel({
+    items: 1,
+    nav: false
+  });
+
+  // Cards carousel
+  $('#cards-owl').owlCarousel({
+    responsive: {
+      768: {
+        items: 2,
+        nav: false
+      }
+    }
+  });
+
+
